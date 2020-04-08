3bd346531810acf38bb4dc37dde9df75daa879ec
schvster <48957858+schvster@users.noreply.github.com>
2020-04-08 13:11:00 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+importScripts('sw-toolbox.js');
+
+toolbox.precache([
+  'companion.js',
+  'index.html',
+  'index.js',
+  'styles.css',
+  './mdl/material.min.css',
+  './mdl/material.min.js'
+]);
